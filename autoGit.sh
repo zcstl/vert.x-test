4ee30dfa93716c08c98acfa3c4ac76bba59badf3
CPE-Developer <hnhkzcs@163.com>
2018-03-15 23:36:48 +0800

    update

--- autoGit.sh
+++ autoGit.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+commitMsg=$1
 #git remote add origin https://github.com/zcstl/leetcode.git
 git add *
-git commit -m "For vert.x learning"
+if [ $commitMsg'x' == 'x' ];then
+   echo git commit -m "auto commit"
+   git commit -m "auto commit"
+else
+   echo git commit -m $commitMsg
+   git commit -m $commitMsg
+fi
 #git push -u origin master
 git push 
